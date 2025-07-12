a9d1c21e449358aed8890e28bcd197ce40fa3155
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-07-12 06:34:15 +0500

    new changes done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
@@ -31,6 +32,7 @@ import MenOrder from "./pages/MenOrder";
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
