4ac16c118dafb7008ce751d1a783a1a735c827d9
Mudasar Shakil <117173890+mudasarasim@users.noreply.github.com>
2025-07-18 21:33:45 +0500

    config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // src/config.js
-const BASE_URL = "http://localhost/aioptimize"; // Change this for production
+const BASE_URL = "https://aioptimizedservices.com"; // Change this for production
 export default BASE_URL;
